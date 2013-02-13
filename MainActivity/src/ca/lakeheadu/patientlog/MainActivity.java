2c94c372d0d48a0eb568fe9777a22596bb728251
Ryan Gallucci <jack_nel@msn.com>
2013-02-13 00:53:08 -0500

    Fixed Rating
    Rating stars only go up by one instead of halves

--- MainActivity/src/ca/lakeheadu/patientlog/MainActivity.java
+++ MainActivity/src/ca/lakeheadu/patientlog/MainActivity.java
@@ -4,14 +4,17 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
 
+public class MainActivity extends Activity {
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+ 
     }
 
+       
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -20,3 +23,4 @@ public class MainActivity extends Activity {
     }
     
 }
+
