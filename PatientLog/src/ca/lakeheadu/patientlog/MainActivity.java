dd1bf0c4677496a964684b1cac9346b071354019
Casey Harding <cjhardin@lakeheadu.ca>
2013-02-18 19:24:09 -0500

    Submit actions. Dynamic Text field.
    I got it so the Submit button will display a little message on screen. I
    also put a text field that changes depending on what star rating the
    user has chosen. You guys may have found another or better way to do
    this. I just thought I'd upload it so you could take a look.

--- PatientLog/src/ca/lakeheadu/patientlog/MainActivity.java
+++ PatientLog/src/ca/lakeheadu/patientlog/MainActivity.java
@@ -3,13 +3,51 @@ package ca.lakeheadu.patientlog;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.RatingBar.OnRatingBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements OnRatingBarChangeListener {
+protected TextView lblRating;
+public int numStars;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+   public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        	//Get Rating Bar Value	
+        	
+        	//Question Page Submit Button Press
+        	Button btnQSubmit =(Button) findViewById(R.id.btnQSubmit);
+        	final RatingBar myRatingBar = (RatingBar)findViewById(R.id.ratingBar1);
+        	lblRating = (TextView)findViewById(R.id.txtRDF);
+        	lblRating.setText("Please pick a star rating. 1 being worst and 5 being best.");
+        	
+        	//Set appropriate listener for change events
+        	myRatingBar.setOnRatingBarChangeListener(this);
+        	btnQSubmit.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					if(myRatingBar.getRating() != 0){
+						//This is where SQL script should go
+						DisplayToast("You have clicked the submit button on the question page!");
+						myRatingBar.setEnabled(true);
+					}
+					else {
+						//This is for when users either don't change the star rating or put it back to zero
+						DisplayToast("Please pick a star rating");
+					}
+				}
+			
+				private void DisplayToast(String msg) {
+					Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
+					
+				}
+			});
+
     }
 
     @Override
@@ -18,5 +56,33 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
+
+	@Override
+	public void onRatingChanged(RatingBar ratingBar1, float arg1, boolean arg2) {
+		RatingBar myRatingBar = (RatingBar)findViewById(R.id.ratingBar1);
+		numStars = (int)myRatingBar.getRating();
+		switch (numStars) {
+		case 1:
+			lblRating.setText("Terrible");
+			break;
+		case 2:
+			lblRating.setText("Bad");
+			break;	
+		case 3:
+			lblRating.setText("Average");
+			break;
+		case 4:
+			lblRating.setText("Good");
+			break;
+		case 5:
+			lblRating.setText("Amazing");
+			break;
+		default:
+			lblRating.setText("Please pick a star rating. 1 being worst and 5 being best.");
+			break;
+		}
+		
+	}
+
+
 }
